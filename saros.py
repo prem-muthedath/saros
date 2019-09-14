73be1b63c50e4fb2f3689436b564ed4bbc9309e9
Prem Muthedath <prem.muthedath@gmail.com>
2019-09-14 19:01:46 +0530

    renamed Document -> DocRevisions, refactored link() [prev called link_revs)], Documents now tracks doc name, renamed methods, etc

--- saros.py
+++ saros.py
@@ -13,13 +13,23 @@ class Documents:
             "GE00-5": [("name", "GE00"), ("rev", 5), ("prev", 4), ("last", 6), ("content", "i am GE00-5")],
             "GE00-6": [("name", "GE00"), ("rev", 6), ("prev", 5), ("last", 6), ("content", "i am GE00-6")],
             "GE01-1": [("name", "GE01"), ("rev", 1), ("prev", 0), ("last", 2), ("content", "i am GE01-1")],
-            "GE01-2": [("name", "GE01"), ("rev", 2), ("prev", 1), ("last", 2), ("content", "i am GE01-2")]
+            "GE01-2": [("name", "GE01"), ("rev", 2), ("prev", 1), ("last", 2), ("content", "i am GE01-2")],
+            "GE02-1": [("name", "GE02"), ("rev", 1), ("prev", 0), ("last", 4), ("content", "i am GE02-1")],
+            "GE02-2": [("name", "GE02"), ("rev", 2), ("prev", 1), ("last", 4), ("content", "i am GE02-2")],
+            "GE02-3": [("name", "GE02"), ("rev", 3), ("prev", 2), ("last", 4), ("content", "i am GE02-3")],
+            "GE02-4": [("name", "GE02"), ("rev", 4), ("prev", 3), ("last", 4), ("content", "i am GE02-4")],
+            "GE02-5": [("name", "GE02"), ("rev", 5), ("prev", 0), ("last", 7), ("content", "i am GE02-5")],
+            "GE02-6": [("name", "GE02"), ("rev", 6), ("prev", 5), ("last", 7), ("content", "i am GE02-6")],
+            "GE02-7": [("name", "GE02"), ("rev", 7), ("prev", 6), ("last", 7), ("content", "i am GE02-7")],
+            "GE03-1": [("name", "GE03"), ("rev", 1), ("prev", 0), ("last", 1), ("content", "i am GE03-1")]
         }
+        self.doc_name=""
 
     def link_revs(self):
         for each in self.doc_names():
-            Document(each).link_revs(self.last_revs(each), self)
-        return self.docs
+            self.doc_name=each
+            DocRevisions().link(self.last_revs(), self)
+        return self.to_str()
 
     def doc_names(self):
         names=[]
@@ -29,26 +39,32 @@ class Documents:
                 names.append(name)
         return names
 
-    def last_revs(self, doc_name):
+    def last_revs(self):
         last_revs=[]
         for key in self.docs:
-            if doc_name in key:
+            if self.doc_name in key:
                 doc_rev=self.docs[key][1][1]
                 last_rev=self.docs[key][3][1]
                 last_revs.append((doc_rev, last_rev))
         return last_revs
 
-    def update_rev_link(self, doc_name, doc_rev, doc):
-        doc_id=doc_name + "-" + str(doc_rev)
+    def to_str(self):
+        val=""
+        for each in sorted(self.docs):
+            val+=each + ": " + str(self.docs[each]) + "\n"
+        return val
+
+    def update_rev_link(self, doc_rev, doc_revs):
+        doc_id=self.doc_name + "-" + str(doc_rev)
         doc_xml= [
             "<id>" + doc_id + "</id>",
-            "<name>" + self.docs[doc_id][0][1] + "</name>",
+            "<name>" + self.doc_name + "</name>",
             "<rev>" + str(self.docs[doc_id][1][1]) + "</rev>",
             "<prev>" + str(self.docs[doc_id][2][1]) + "</prev>",
             "<last>" + str(self.docs[doc_id][3][1]) + "</last>",
             "<content>" + self.docs[doc_id][4][1] + "</content>"
         ]
-        doc.update(doc_xml)
+        doc_revs.update(doc_xml)
         self.load(doc_xml)
         self.update_last_rev(doc_id)
 
@@ -59,7 +75,7 @@ class Documents:
             if each.startswith("<id>"):
                 doc_id=self.parse(each, "<id>")
             elif each.startswith("<name>"):
-                vals.append(("name", self.parse(each, "<name>")))
+                vals.append(("name", self.doc_name))
             elif each.startswith("<rev>"):
                 vals.append(("rev", int(self.parse(each, "<rev>"))))
             elif each.startswith("<prev>"):
@@ -77,42 +93,44 @@ class Documents:
         return xml_str[start:end]
 
     def update_last_rev(self, doc_id):
-        name=doc_id.split("-")[0]
         last_rev=self.docs[doc_id][3][1]
         for each in self.docs:
-            if each != doc_id and each.startswith(name):
+            if each != doc_id and each.startswith(self.doc_name):
                 self.docs[each][3] = ("last", last_rev)
 
-class Document:
-    def __init__(self, name):
-        self.me = { "name": name, "rev": -1, "prev": -1, "last": -1 } 
 
-    def link_revs(self, last_revs, docs):
-        fst, snd = [], []
-        last=-1
+class DocRevisions:
+    def __init__(self):
+        self.me = { "rev": -1, "prev": -1, "last": -1 }
+
+    def link(self, last_revs, docs):
+       # last_revs = [ (rev, last), .., (rev, last) ]
+       # rev_chains = { last1: [rev, rev, .., rev],
+       #                last2: [rev, rev, .., rev] }
+       # algorithm groups revisions by the last revision they point to
+        rev_chains={}
         for each in last_revs:
-            if each[1] > last:
-                if last == -1:
-                    last=each[1]
-                    fst.append(each[0])
-                elif last > 0:
-                    last=each[1]
-                    snd.append(each[0])
-            elif each[1] == last:
-                if len(snd) == 0:
-                    fst.append(each[0])
-                else:
-                    snd.append(each[0])
-        fst.sort(), snd.sort()
-        if len(fst) == 0 or len(snd) == 0: return
-        if fst[0] < snd[0]:
-            self.me["rev"]=snd[0]
-            self.me["prev"]=fst[-1]
-        elif snd[0] < fst[0]:
-            self.me["rev"]=snd[-1]
-            self.me["prev"]=fst[0]
-        self.me["last"]=last
-        docs.update_rev_link(self.me["name"], self.me["rev"], self)
+            if each[1] not in rev_chains:
+                rev_chains[each[1]]=[each[0]]
+            else:
+                rev_chains[each[1]].append(each[0])
+        if len(rev_chains) > 2:
+            raise RuntimeError("> 2 rev chains found")
+        if len(rev_chains) < 2:
+            return    # no broken links, so skip
+        keys=rev_chains.keys()
+        for key in keys:
+            rev_chains[key].sort()
+        if rev_chains[keys[0]] < rev_chains[keys[1]]:
+            self.me["rev"]=rev_chains[keys[1]][0]
+            self.me["prev"]=rev_chains[keys[0]][-1]
+            self.me["last"]=keys[1]
+        elif rev_chains[keys[1]] < rev_chains[keys[0]]:
+            self.me["rev"]=rev_chains[keys[1]][-1]
+            self.me["prev"]=rev_chains[keys[0]][0]
+            self.me["last"]=keys[0]
+        docs.update_rev_link(self.me["rev"], self)
+
 
     def update(self, my_xml):
         for index, each in enumerate(my_xml):
@@ -125,6 +143,6 @@ class Document:
 
 
 
-for x, y in Documents().link_revs().items():
-    print(x, y)
-        
+print(Documents().link_revs())
+ 
+
