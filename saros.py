a34366fb8fafed8fff4567643f26853ae5beea1a
Prem Muthedath <prem.muthedath@gmail.com>
2019-09-16 18:05:02 +0530

    made class and methods, where needed, private, using _ & __; added __main__()

--- saros.py
+++ saros.py
@@ -5,7 +5,7 @@
 
 class Saros:
     def __init__(self):
-        self.docs = {
+        self.__docs = {
             "JE00-1": [("name", "JE00"), ("rev", 1), ("prev", 0), ("last", 3), ("content", "i am JE00-1")],
             "JE00-2": [("name", "JE00"), ("rev", 2), ("prev", 1), ("last", 3), ("content", "i am JE00-2")],
             "JE00-3": [("name", "JE00"), ("rev", 3), ("prev", 2), ("last", 3), ("content", "i am JE00-3")],
@@ -25,98 +25,97 @@ class Saros:
             "JE02-7": [("name", "JE02"), ("rev", 7), ("prev", 6), ("last", 7), ("content", "i am JE02-7")],
             "JE03-1": [("name", "JE03"), ("rev", 1), ("prev", 0), ("last", 1), ("content", "i am JE03-1")]
         }
-        self.doc_name=""
+        self.__doc_name=""
+
 
     def link_revs(self):
-        print("BEFORE: \n" + self.to_str())
-        for each in self.doc_names():
-            self.doc_name=each
-            DocRevisionChains().link(self.last_revs(), self)
-        return self.to_str()
+        for each in self.__doc_names():
+            self.__doc_name=each
+            _DocRevisionChains()._link(self.__last_revs(), self)
 
-    def doc_names(self):
+    def to_str(self):
+        val=""
+        for each in sorted(self.__docs):
+            val+=each + ": " + str(self.__docs[each]) + "\n"
+        return val
+
+    def __doc_names(self):
         names=[]
-        for doc_id in self.docs:
-            name=self.fetch(doc_id, "name")
+        for doc_id in self.__docs:
+            name=self.__fetch(doc_id, "name")
             if name not in names:
                 names.append(name)
         return names
 
-    def last_revs(self):
+    def __last_revs(self):
         last_revs=[]
-        for doc_id in self.docs:
-            if self.doc_name in doc_id:
-                doc_rev=self.fetch(doc_id, "rev")
-                last_rev=self.fetch(doc_id, "last")
+        for doc_id in self.__docs:
+            if self.__doc_name in doc_id:
+                doc_rev=self.__fetch(doc_id, "rev")
+                last_rev=self.__fetch(doc_id, "last")
                 last_revs.append((doc_rev, last_rev))
         return last_revs
 
-    def to_str(self):
-        val=""
-        for each in sorted(self.docs):
-            val+=each + ": " + str(self.docs[each]) + "\n"
-        return val
-
-    def update_rev_link(self, doc_revs, doc_rev_chains):
+    def _update_rev_link(self, doc_revs, doc_rev_chains):
         doc_id=""
         doc_xmls=[]
         for doc_rev in doc_revs:
-            doc_id=self.doc_name + "-" + str(doc_rev)
-            doc_xmls.append(self.doc_xml(doc_id))
-        doc_rev_chains.update(doc_xmls)
+            doc_id=self.__doc_name + "-" + str(doc_rev)
+            doc_xmls.append(self.__doc_xml(doc_id))
+        doc_rev_chains._update(doc_xmls)
         for doc_xml in doc_xmls:
-            self.load(doc_xml)
-        self.update_last_rev(doc_id)
+            self.__load(doc_xml)
+        self.__update_last_rev(doc_id)
 
-    def doc_xml(self, doc_id):
+    def __doc_xml(self, doc_id):
         return [
             "<id>" + doc_id + "</id>",
-            "<name>" + self.doc_name + "</name>",
-            "<rev>" + str(self.fetch(doc_id, "rev")) + "</rev>",
-            "<prev>" + str(self.fetch(doc_id, "prev")) + "</prev>",
-            "<last>" + str(self.fetch(doc_id, "last")) + "</last>",
-            "<content>" + self.fetch(doc_id, "content") + "</content>"
+            "<name>" + self.__doc_name + "</name>",
+            "<rev>" + str(self.__fetch(doc_id, "rev")) + "</rev>",
+            "<prev>" + str(self.__fetch(doc_id, "prev")) + "</prev>",
+            "<last>" + str(self.__fetch(doc_id, "last")) + "</last>",
+            "<content>" + self.__fetch(doc_id, "content") + "</content>"
         ]
 
-    def load(self, doc_xml):
+    def __load(self, doc_xml):
         doc_id=""
         vals = []
         for each in doc_xml:
             if each.startswith("<id>"):
-                doc_id=self.parse(each, "<id>")
+                doc_id=self.__parse(each, "<id>")
             elif each.startswith("<name>"):
-                vals.append(("name", self.doc_name))
+                vals.append(("name", self.__doc_name))
             elif each.startswith("<rev>"):
-                vals.append(("rev", int(self.parse(each, "<rev>"))))
+                vals.append(("rev", int(self.__parse(each, "<rev>"))))
             elif each.startswith("<prev>"):
-                vals.append(("prev", int(self.parse(each, "<prev>"))))
+                vals.append(("prev", int(self.__parse(each, "<prev>"))))
             elif each.startswith("<last>"):
-                vals.append(("last", int(self.parse(each, "<last>"))))
+                vals.append(("last", int(self.__parse(each, "<last>"))))
             elif each.startswith("<content>"):
-                vals.append(("content", self.parse(each, "<content>")))
-        self.docs.pop(doc_id)
-        self.docs[doc_id]=vals
+                vals.append(("content", self.__parse(each, "<content>")))
+        self.__docs.pop(doc_id)
+        self.__docs[doc_id]=vals
 
-    def parse(self, xml_str, xml_tag):
+    def __parse(self, xml_str, xml_tag):
         start=len(xml_tag)
         end=len(xml_str)-(start+1)
         return xml_str[start:end]
 
-    def update_last_rev(self, doc_id):
-        last_rev=self.fetch(doc_id, "last")
-        for _id in self.docs:
-            if _id!= doc_id and _id.startswith(self.doc_name):
-                self.put(_id, "last", last_rev)
+    def __update_last_rev(self, doc_id):
+        last_rev=self.__fetch(doc_id, "last")
+        for _id in self.__docs:
+            if _id!= doc_id and _id.startswith(self.__doc_name):
+                self.__put(_id, "last", last_rev)
 
-    def fetch(self, doc_id, col):
-        doc=self.docs[doc_id]
+    def __fetch(self, doc_id, col):
+        doc=self.__docs[doc_id]
         for (_col, _val) in doc:
             if _col == col: return _val
         raise RuntimeError("fetch failure for doc_id: " + \
                             doc_id + " col: " + col + " not found")
 
-    def put(self, doc_id, col, val):
-        doc=self.docs[doc_id]
+    def __put(self, doc_id, col, val):
+        doc=self.__docs[doc_id]
         for index, (_col, _val) in enumerate(doc):
             if _col == col:
                 doc[index]=(col, val)
@@ -125,12 +124,12 @@ class Saros:
                 " col: " + col + " not found")
 
 
-class DocRevisionChains:
+class _DocRevisionChains:
     def __init__(self):
-        # self.broken_links = { rev: (prev, last) }
-        self.broken_links = {}
+        # self.__broken_links = { rev: (prev, last) }
+        self.__broken_links = {}
 
-    def link(self, last_revs, saros):
+    def _link(self, last_revs, saros):
         # last_revs = [ (rev, last), .., (rev, last) ]
         # rev_chains = { last1: [rev, rev, .., rev],
         #                last2: [rev, rev, .., rev] }
@@ -143,10 +142,10 @@ class DocRevisionChains:
                 rev_chains[last].append(rev)
         if len(rev_chains) < 2:
             return    # no broken links, so skip
-        self.extract_broken_links(rev_chains)
-        saros.update_rev_link(sorted(self.broken_links), self)
+        self.__extract_broken_links(rev_chains)
+        saros._update_rev_link(sorted(self.__broken_links), self)
 
-    def extract_broken_links(self, rev_chains):
+    def __extract_broken_links(self, rev_chains):
         lasts=sorted(rev_chains)
         last=lasts[-1]
         for each in lasts:
@@ -161,15 +160,15 @@ class DocRevisionChains:
             prev_last=lasts[index]
             rev=rev_chains[each][0]
             prev=rev_chains[prev_last][-1]
-            self.broken_links[rev]=(prev, last)
+            self.__broken_links[rev]=(prev, last)
 
-    def update(self, xmls):
-        revs=sorted(self.broken_links)
+    def _update(self, xmls):
+        revs=sorted(self.__broken_links)
         for rev, xml in zip(revs, xmls):
-            self.update_link(rev, xml)
+            self.__update_link(rev, xml)
 
-    def update_link(self, rev, xml):
-        prev,last=self.fetch(rev)
+    def __update_link(self, rev, xml):
+        prev,last=self.__fetch(rev)
         for index, each in enumerate(xml):
             if each.startswith("<rev>"):
                 xml[index]="<rev>"+str(rev)+"</rev>"
@@ -178,11 +177,14 @@ class DocRevisionChains:
             elif each.startswith("<last>"):
                 xml[index]="<last>"+str(last)+"</last>"
 
-    def fetch(self, rev):
-        return self.broken_links[rev]
+    def __fetch(self, rev):
+        return self.__broken_links[rev]
 
 
+if __name__ == "__main__":
+    saros=Saros()
+    print("SAROS REPOSITORY STATE BEFORE: \n" + saros.to_str())
+    saros.link_revs()
+    print("SAROS REPOSITORY STATE AFTER: \n" + saros.to_str())
 
-print(Saros().link_revs())
- 
 
