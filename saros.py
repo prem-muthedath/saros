ded78ca34468c6c63fdf4a17fe20396915760973
Prem Muthedath <prem.muthedath@gmail.com>
2019-09-17 18:08:00 +0530

    Detailed description of Saros; header comments for classes; added/edited other comments; renamed method update_rev_lin() -> update_rev_links()

--- saros.py
+++ saros.py
@@ -1,10 +1,84 @@
 #!/usr/bin/python
 
-# Prem: this code, written in python, links document revisions in saros, a
-# document repository
+# Prem: this code, written in python, links document revisions in Saros, a
+# fictitious document repository.
+#
+################################################################################
+# NOTE:
+# 1. Saros stores documents and their revisions.
+# 2. Each document ("doc") has:
+#       -> name
+#       -> revision number ("rev")
+#       -> immediate preceding revision ("prev")
+#       -> content
+#       -> the latest revision number ("last") ~ total # of revisions
+# 3. Revision numbers start from 1, & increase by 1.
+# 4. Example -- if doc "foo" has been revised 7 times, then its 4th revision:
+#       -> name: foo
+#       -> rev:  4
+#       -> prev: 3
+#       -> last: 7
+#       -> content: "hey, i am foo"
+# 5. For the example in (4), the end cases will be:
+#       -> rev=1 => prev=0, last=7
+#       -> rev=7 => prev=6, last=7
+# 6. In Saros, no 2 docs can have the same "name" & "rev". For example:
+#       -> "foo" rev=2 -- possible
+#       -> "foo" rev=3 -- possible
+#       -> "foo" rev=4 -- possible
+#       -> "foo" rev=2 -- repeat NOT possible
+# 7. Saros indexes docs:
+#       -> using a generated unique document id
+#       -> an id refers to a doc with unique combination of "name" & "rev"
+# 8. Saros requires that all "rev"s of a doc with the same "name" must have:
+#       -> valid & unique "prev"s
+#       -> same "last" ~ same total # of revisions
+# 9. Saros, however, is not perfect -- some docs have broken revision links
+# 10. Terminology [ see example in (4) ]:
+#       -> revision links & revision chains refer to a doc with same "name"
+#       -> revision link: ("prev", "rev", "last")  => (3, 4, 7)
+#       -> revision chain: linked "rev"s with same "last" => [1,2,3,4,5,6,7]
+# 11. A broken revision link is where:
+#       -> a "rev" does not have a valid & unique "prev"
+#       -> "last" not ~ "last" of other links, so we've > 1 revision chain
+# 12. Example -- broken revision link:
+#       -> doc "goo" has total 7 revisions
+#       -> rev 1 => prev=0, last=3
+#       -> rev 2 => prev=1, last=3
+#       -> rev 3 => prev=2, last=3
+#
+#       -> rev 4 => prev=0, last=7  --> broken: prev != 3
+#       -> rev 5 => prev=4, last=7
+#       -> rev 6 => prev=5, last=7
+#       -> rev 7 => prev=6, last=7
+#
+#       -> links don't have same "last", so we've 2 revision chains:
+#           -> for last=3 => [1, 2, 3]
+#           -> for last=7 => [4, 5, 6]
+# 13. To fix the problem in (12), we must:
+#       (a) spot the "rev" with broken link -> rev 4
+#       (b) determine its right "prev" value -> "prev"=3
+#       (c) determine its right "last" value -> "last"=7
+#       (d) update Saros with (a), (b), & (c)
+#       (e) Saros will then update all "rev"s "last" to same value -> "last"=7
+# 14. To do 13 (a), (b), & (c) [ see example in (12) ]:
+#       -> call Saros API "last_revs()" with a doc "name" -- say, "goo"
+#       -> you get [(rev, last)] -- [(1,3),(3,3),(2,3),(5,7),(4,7),(7,7),(6,7)]
+#       -> use this unordered data to do 13 (a), (b), & (c)
+# 15.  But Saros has a quirk -- To do 13 (d):
+#       --> Saros has no API to update "prev" & "last"
+#       --> instead, you must request Saros for a full XML dump of broken "rev"
+#       --> you then update the XML with data from 13 (a), (b), & (c)
+#       --> send modified XML to Saros to load
+#       --> Saros then parses XML & loads data
+################################################################################
+
 
 class Saros:
+    # this class models Saros document repository.  it is the only public class 
+    # in this module. it works with a private class to link document revisions.
     def __init__(self):
+        # self_docs = { doc_id: [ ("name", val), ("rev", val), ("prev", val), ("content", val) ] }
         self.__docs = {
             "JE00-1": [("name", "JE00"), ("rev", 1), ("prev", 0), ("last", 3), ("content", "i am JE00-1")],
             "JE00-2": [("name", "JE00"), ("rev", 2), ("prev", 1), ("last", 3), ("content", "i am JE00-2")],
@@ -25,7 +99,7 @@ class Saros:
             "JE02-7": [("name", "JE02"), ("rev", 7), ("prev", 6), ("last", 7), ("content", "i am JE02-7")],
             "JE03-1": [("name", "JE03"), ("rev", 1), ("prev", 0), ("last", 1), ("content", "i am JE03-1")]
         }
-        self.__doc_name=""
+        self.__doc_name=""  # current document whose revisions are being linked
 
 
     def link_revs(self):
@@ -48,6 +122,7 @@ class Saros:
         return names
 
     def __last_revs(self):
+        # returns an unordered [ ("rev", "last") ]
         last_revs=[]
         for doc_id in self.__docs:
             if self.__doc_name in doc_id:
@@ -56,7 +131,7 @@ class Saros:
                 last_revs.append((doc_rev, last_rev))
         return last_revs
 
-    def _update_rev_link(self, doc_revs, doc_rev_chains):
+    def _update_rev_links(self, doc_revs, doc_rev_chains):
         doc_id=""
         doc_xmls=[]
         for doc_rev in doc_revs:
@@ -125,6 +200,8 @@ class Saros:
 
 
 class _DocRevisionChains:
+    # builds doc revision chains, spots broken revision links, & works with 
+    # Saros to link them.
     def __init__(self):
         # self.__broken_links = { rev: (prev, last) }
         self.__broken_links = {}
@@ -133,7 +210,8 @@ class _DocRevisionChains:
         # last_revs = [ (rev, last), .., (rev, last) ]
         # rev_chains = { last1: [rev, rev, .., rev],
         #                last2: [rev, rev, .., rev] }
-        # algorithm groups revisions by the last revision they point to
+        # algorithm groups revisions by their last revision.  Each group is a 
+        # revision chain, & if we've > 1, we've broken revision links.
         rev_chains={}
         for (rev, last) in last_revs:
             if last not in rev_chains:
@@ -143,7 +221,7 @@ class _DocRevisionChains:
         if len(rev_chains) < 2:
             return    # no broken links, so skip
         self.__extract_broken_links(rev_chains)
-        saros._update_rev_link(sorted(self.__broken_links), self)
+        saros._update_rev_links(sorted(self.__broken_links), self)
 
     def __extract_broken_links(self, rev_chains):
         lasts=sorted(rev_chains)
@@ -153,8 +231,8 @@ class _DocRevisionChains:
         for index, each in enumerate(lasts[1:]):
             # scan all rev chains for broken rev links; gather & link them.
             # broken rev link is where a rev is without a valid prev.
-            # 1st rev in a rev chain is a broken link, because it has no prev
-            # skip first rev chain, as its 1st rev can not have a prev.
+            # 1st rev in a rev chain is a broken link, because it has no prev.
+            # skip first rev chain, however, as its 1st rev can not have a prev.
             # the 1st rev in all other rev chains is a broken rev link.
             # index starts @ 0, but we loop from [1:], so index -> prev item.
             prev_last=lasts[index]
